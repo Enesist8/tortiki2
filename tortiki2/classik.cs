4681f2cc202507b51557bff9b8d31e738f74841d
Enesist) <Enesist)@ENESIST>
2022-11-07 15:01:11 +0300

    Добавьте файлы проекта.

---
+++ tortiki2/classik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tortiki
+{
+    internal class tort
+    {
+        public string Forma;
+        public string Vkys;
+        public string Razmer;
+        public int Korzi;
+        public string Glazyr;
+        public string Dekor;
+        public string Cena;
+
+
+    }
+}
